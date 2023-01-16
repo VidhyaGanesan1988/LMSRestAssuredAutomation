9cd81ba2129d3fb8270a78b472bf883941c5026d
matesapna20 <109248778+matesapna20@users.noreply.github.com>
2023-01-15 20:14:30 -0800

    Update Config.java

--- src/test/java/utilities/Config.java
+++ src/test/java/utilities/Config.java
@@ -3,8 +3,19 @@ package utilities;
 public class Config {
 	
 	public static final String BASE_URL = "https://lms-backend-service.herokuapp.com/lms";
+	public static final String BASE_Invalid_URL = "https://lms-backend.herokuapp.com/lms";
 	public static final String GetAllProgram_URL = "https://lms-backend-service.herokuapp.com/lms/allPrograms";
 	public static final String GetSingleProgram_URL = "https://lms-backend-service.herokuapp.com/lms/programs";
+	
+	public static final String Invalid_Endpoint_PutProgram = "/putprogrammmmmm/329";
+	public static final String Valid_Endpoint_PutProgram = "/putprogram/4421";
+	public static final String ProgramId_NotOnServer = "/putprogram/8652";
+	
+	public static final String ValidEndPoint_ProgramName = "/program/jan23SDET1";
+	public static final String InValidEndPoint_ProgramName = "/program/SDET30255696563333333333";
+	
+	public static final String Valid_BatchPut = "/batches/467";
+	public static final String Invalid_BatchPut = "/batchessssssss/1456";
 
 	public static final String EXCEL = "C:/Users/admin/eclipse-workspace/LMSRestAssuredAutomation/Data Files/LMSAPIHackathon.xlsx";
 }
